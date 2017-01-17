7a80b68b2933ad53b0ba5bf1d0ab306618ed0adc
Kimiko <kimikomotoyama@gmail.com>
2017-01-17 13:54:49 +0000

    Twitterプロフィール機能

--- elites-twitter/app/controllers/users_controller.rb
+++ elites-twitter/app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
   def show
+    @tweets = User.find(params[:id]).tweets.order('updated_at DESC').page(params[:page])
+    @user = User.find(params[:id])
   end
 end
